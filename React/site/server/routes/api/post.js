64407fca7fe3ea9b041349fada41e5d00b741d08
DevAyoung <amdagy@gmail.com>
2021-01-19 21:33:19 +0900

    ADD POSTWRITE ROUTE

--- React/site/server/routes/api/post.js
+++ React/site/server/routes/api/post.js
@@ -4,7 +4,48 @@ import auth from '../../middleware/auth';
 //Model
 import Post from '../../models/post';
 
-const router = express.Router()
+const router = express.Router();
+
+import multer from 'multer';
+import multers3 from 'multer-s3';
+import path from 'path';
+import AWS from 'aws-sdk';
+import dotenv from 'dotenv';
+dotenv.config()
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_PRIVATE_KEY
+});
+
+const uploadS3 = multer ({
+    storage: multers3({
+        s3,
+        bucket: "ayoungblog/upload",
+        region: "ap-northeast-e",
+        key(req, file, cb){
+            const ext = path.extname(file.originalname);
+            const basename = path.basename = path.basename(file.originalname, ext);
+            cb(null, basename + new Date().valueOf() + ext)
+        }
+    }),
+    limits: {fileSize: 100 * 1024 * 1024},
+});
+
+// @route       POST api/post/image
+// @desc        Create a Post
+// @access      Private
+
+router.post("/image", uploadS3.array("upload",5), async(req, res, next) => {
+    try {
+        console.log(req.files.map((v) => v.location))
+        res.json({uploaded: true, url: req.files.map((v) => v.location) });
+    } catch (e) {
+        console.error(e);
+        res.json({uploaded:false, url: null})
+    }
+});
+
 
 // api/post
 router.get('/', async(req, res)=> {
